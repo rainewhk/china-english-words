a68a7af384eabc669b4ada6f8a57984a58e4f512
RainPPR <PPR2125773894@163.com>
2026-05-27 18:43:49 +0800

    Update

--- main.py
+++ main.py
@@ -14,26 +14,26 @@ def exec_pdf(pdf_name):
 if __name__ == '__main__':
     # 补充新的需要手动设置
     file_name_list = [
-        # 人教版
-        "初中 人教版 七年级上册",
-        "初中 人教版 七年级下册",
-        "初中 人教版 八年级上册",
-        "初中 人教版 八年级下册",
-        "初中 人教版 九年级全一册",
-        # 译林版
-        "初中 译林版 七年级上册",
-        "初中 译林版 七年级下册",
-        "初中 译林版 八年级上册",
-        "初中 译林版 八年级下册",
-        "初中 译林版 九年级上册",
-        "初中 译林版 九年级下册",
-        # 外研社版
-        "初中 外研社版 七年级上册",
-        "初中 外研社版 七年级下册",
-        "初中 外研社版 八年级上册",
-        "初中 外研社版 八年级下册",
-        "初中 外研社版 九年级上册",
-        "初中 外研社版 九年级下册",
+        # # 人教版
+        # "初中 人教版 七年级上册",
+        # "初中 人教版 七年级下册",
+        # "初中 人教版 八年级上册",
+        # "初中 人教版 八年级下册",
+        # "初中 人教版 九年级全一册",
+        # # 译林版
+        # "初中 译林版 七年级上册",
+        # "初中 译林版 七年级下册",
+        # "初中 译林版 八年级上册",
+        # "初中 译林版 八年级下册",
+        # "初中 译林版 九年级上册",
+        # "初中 译林版 九年级下册",
+        # # 外研社版
+        # "初中 外研社版 七年级上册",
+        # "初中 外研社版 七年级下册",
+        # "初中 外研社版 八年级上册",
+        # "初中 外研社版 八年级下册",
+        # "初中 外研社版 九年级上册",
+        # "初中 外研社版 九年级下册",
         # 冀教版
         "初中 冀教版 七年级上册",
         "初中 冀教版 七年级下册",
@@ -83,12 +83,19 @@ if __name__ == '__main__':
     # 从 words_set.txt 恢复状态
 
     for file_name in file_name_list:
-        words_set = set()
-        with open('words_set.txt', 'r', encoding='utf-8') as f:
-            for line in f:
-                words_set.add(line.strip())
-        words_set.update(exec_pdf(file_name))
-        unique_list = sorted(list(words_set))
-        with open('words_set.txt', 'w', encoding='utf-8') as f:
-            for word in unique_list:
-                f.write(word + '\n')
+        pdf_path = f'{root_name}/{file_name}.pdf'
+        txt_path = f'{root_name}/{file_name}.txt'
+        json_path = f'{root_name}/{file_name}'
+        # extract_text(pdf_path, txt_path)
+        print(file_name, end='...')
+        extract_words(txt_path, json_path)
+        print(' ok')
+        # words_set = set()
+        # with open('words_set.txt', 'r', encoding='utf-8') as f:
+        #     for line in f:
+        #         words_set.add(line.strip())
+        # words_set.update(exec_pdf(file_name))
+        # unique_list = sorted(list(words_set))
+        # with open('words_set.txt', 'w', encoding='utf-8') as f:
+        #     for word in unique_list:
+        #         f.write(word + '\n')
